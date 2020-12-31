dd58aaf32633f7e59c980124d53d6c846a72962b
Grimaldo <rgrimaldo2307@gmail.com>
2020-12-31 01:31:56 -0500

    Actualizacion del proyecto al repositorio

---
+++ src/interfaz_proyectofinal/Interfaz_Proyectofinal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz_proyectofinal;
+
+import vista.VistaPrincipal;
+
+/**
+ *
+ * @author asha2
+ */
+public class Interfaz_Proyectofinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VistaPrincipal ventana = new VistaPrincipal();
+        
+        ventana.setVisible(true);
+        
+    }
+    
+}
